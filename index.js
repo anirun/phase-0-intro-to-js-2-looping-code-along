f5697fe4f754d51a06e77fb07a646be9af41067d
Nurina <nurina.junus@gmail.com>
2021-04-12 17:07:58 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i= 0 ; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+function writeCards(array, event) {
+    const cardMsgs = [];
+    for (let i= 0 ; i < array.length ; i++) {
+        cardMsgs.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardMsgs;
+}
+
+
+function countDown (i) {
+    while (i >= 0) {
+        console.log(i);
+        i-=1;
+    }
+}
